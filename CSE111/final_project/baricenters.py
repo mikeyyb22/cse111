8e4669d60b8581749342f39797cf52135f97a3bc
mikeyyb22 <mcgeheemichael22@gmail.com>
2026-02-16 11:42:27 -0300

    csv complete

---
+++ CSE111/final_project/baricenters.py
@@ -0,0 +1,20 @@
+import math
+import tkinter as tk
+
+def main():
+    return
+
+def read_from_csv(file):
+    return
+
+def calculate_r1(first_body, second_body):
+    return
+
+def calculate_r2(r1, distance_au):
+    return
+
+def create_body(mass, distance_au, second_body):
+    return
+
+if __name__ == "__main__":
+    main()
